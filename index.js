d0c35a638efd6653409db0479b40caf19ad30a14
JasmineBanks <banks.012jasmine@gmail.com>
2018-07-02 21:22:07 -0500

    Javascript continues

---
+++ index.js
@@ -0,0 +1,16 @@
+var title = document.querySelector('h1');
+
+var greetUser = function greetUser(){
+    var userName = prompt('What is your name?');
+
+    if(userName !== ''){
+        title.textContent += '. Welcome, ' + userName;
+    }
+    else{
+        greetUser();
+    }
+};
+
+greetUser();
+
+
